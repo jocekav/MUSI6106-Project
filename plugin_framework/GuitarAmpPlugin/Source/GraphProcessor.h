69066f7c92a577a16fc91a86da6c3700c0dd68a1
VedantKalbag <vedant.kalbag@gmail.com>
2022-03-15 23:11:51 -0400

    Added distortion node, fixed distortion class

--- plugin_framework/GuitarAmpPlugin/Source/GraphProcessor.h
+++ plugin_framework/GuitarAmpPlugin/Source/GraphProcessor.h
@@ -31,6 +31,8 @@ public:
     juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout()
     {
         juce::AudioProcessorValueTreeState::ParameterLayout layout;
+        // Input Gain Params
+        layout.add(std::make_unique<juce::AudioParameterFloat>("inputGain", "Input", juce::NormalisableRange<float>(-20, 40, 0.1, 1), 0));
 
         // Noise Gate Params
         layout.add(std::make_unique<juce::AudioParameterFloat>("gateThreshold", "Threshold", juce::NormalisableRange<float>(-40.f, 0.f), 0.f,"dB"));
@@ -39,24 +41,36 @@ public:
         layout.add(std::make_unique<juce::AudioParameterFloat>("gateRelease", "Release", juce::NormalisableRange<float>(0.f, 1000.f), 25.f,"ms"));
 
         // EQ Params
-        layout.add(std::make_unique<juce::AudioParameterFloat>("LowCutFreq", "LowCutFreq", juce::NormalisableRange<float>(20, 20000, 1, 0.25), 20));
-        layout.add(std::make_unique<juce::AudioParameterFloat>("HighCutFreq", "HighCutFreq", juce::NormalisableRange<float>(20, 20000, 1, 0.25), 20000));
-        layout.add(std::make_unique<juce::AudioParameterFloat>("PeakFreq", "PeakFreq", juce::NormalisableRange<float>(20, 20000, 1, 0.25), 750));
-        layout.add(std::make_unique<juce::AudioParameterFloat>("PeakGain", "PeakGain", juce::NormalisableRange<float>(-24, 24, 0.5, 1), 0));
-        layout.add(std::make_unique<juce::AudioParameterFloat>("PeakQ", "PeakQ", juce::NormalisableRange<float>(0.1, 10, 0.05, 1), 1));
         juce::StringArray stringArray;
         for (int i = 0; i < 4; i++)
         {
             juce::String buildStr;
             buildStr << (12 + (i * 12));
-            buildStr << " db/Oct";
+            buildStr << " dB/Oct";
             stringArray.add(buildStr);
         }
         layout.add(std::make_unique<juce::AudioParameterChoice>("LowCutSlope", "LowCutSlope", stringArray, 0));
+        layout.add(std::make_unique<juce::AudioParameterFloat>("LowCutFreq", "LowCutFreq", juce::NormalisableRange<float>(20, 20000, 1, 0.25), 20));
         layout.add(std::make_unique<juce::AudioParameterChoice>("HighCutSlope", "HighCutSlope", stringArray, 0));
-        
-        // Gain Params
-        layout.add(std::make_unique<juce::AudioParameterFloat>("InputGain", "InputGain", juce::NormalisableRange<float>(-20, 40, 0.1, 1), 0));
+        layout.add(std::make_unique<juce::AudioParameterFloat>("HighCutFreq", "HighCutFreq", juce::NormalisableRange<float>(20, 20000, 1, 0.25), 20000));
+        layout.add(std::make_unique<juce::AudioParameterFloat>("PeakFreq", "PeakFreq", juce::NormalisableRange<float>(20, 20000, 1, 0.25), 750));
+        layout.add(std::make_unique<juce::AudioParameterFloat>("PeakGain", "PeakGain", juce::NormalisableRange<float>(-24, 24, 0.5, 1), 0));
+        layout.add(std::make_unique<juce::AudioParameterFloat>("PeakQ", "PeakQ", juce::NormalisableRange<float>(0.1, 10, 0.05, 1), 1));
+
+        // Amplifier Params
+        // drive, mix, distortiontype
+        // preLP freq, pre HP freq, pre peak freq
+        // post lp freq, post hp freq, post peak freq
+        layout.add(std::make_unique<juce::AudioParameterFloat>("ampDrive", "Drive", juce::NormalisableRange<float>(0.f, 40.f), 20.f,""));
+        layout.add(std::make_unique<juce::AudioParameterFloat>("ampBlend", "Blend", juce::NormalisableRange<float>(0.f, 100.f), 100.f,"%"));
+        juce::StringArray strArray ( {"None", "Tanh", "ATan", "Hard Clipper", "Rectifier", "Sine", "Tube"});
+        layout.add(std::make_unique<juce::AudioParameterChoice>("ampType", "Distortion Type", strArray, 1));
+        layout.add(std::make_unique<juce::AudioParameterFloat>("ampPreLPF", "Pre-Amp LPF", juce::NormalisableRange<float>(20, 20000, 1, 0.5), 20000.f, "Hz"));
+        layout.add(std::make_unique<juce::AudioParameterFloat>("ampPreHPF", "Pre-Amp HPF", juce::NormalisableRange<float>(20, 20000, 1, 0.5), 20.f, "Hz"));
+        layout.add(std::make_unique<juce::AudioParameterFloat>("ampEmphasis", "Emphasis Frequency", juce::NormalisableRange<float>(20, 20000, 1, 0.5), 700.f, "Hz"));
+        layout.add(std::make_unique<juce::AudioParameterFloat>("ampEmphasisGain", "Emphasis Filter Gain", juce::NormalisableRange<float>(-20.f, 20.f), 0.f,"dB"));
+        layout.add(std::make_unique<juce::AudioParameterFloat>("ampPostLPF", "Post-Amp LPF", juce::NormalisableRange<float>(20, 20000, 1, 0.5), 20000.f, "Hz"));
+        layout.add(std::make_unique<juce::AudioParameterFloat>("ampPostHPF", "Post-Amp HPF", juce::NormalisableRange<float>(20, 20000, 1, 0.5), 20.f, "Hz"));
 
         // Reverb Params
         layout.add(std::make_unique<juce::AudioParameterFloat>("rvbBlend", "Wet/Dry", juce::NormalisableRange<float>(0.f, 1.f), 0.25f,"%"));
@@ -71,12 +85,14 @@ public:
         layout.add(std::make_unique<juce::AudioParameterFloat>("compMakeupGain", "Makeup Gain", juce::NormalisableRange<float>(0.f, 40.f), 0.f,"dB"));
 
         // Phaser Params
-        layout.add(std::make_unique<juce::AudioParameterFloat>("phaserRate", "Rate", juce::NormalisableRange<float>(0.f, 2000.f), 0.f,"Hz"));
+        layout.add(std::make_unique<juce::AudioParameterFloat>("phaserRate", "Rate", juce::NormalisableRange<float>(0.1f, 20.f), 0.f,"Hz"));
         layout.add(std::make_unique<juce::AudioParameterFloat>("phaserDepth", "Depth", juce::NormalisableRange<float>(0.f, 1.f), 0.25f,""));
         layout.add(std::make_unique<juce::AudioParameterFloat>("phaserFc", "Centre Frequency", juce::NormalisableRange<float>(0.f, 1000.f, 1, 0.25), 25.f,"Hz"));
         layout.add(std::make_unique<juce::AudioParameterFloat>("phaserFeedback", "Feedback", juce::NormalisableRange<float>(-1.f, 1.f), 0.f,"%"));
         layout.add(std::make_unique<juce::AudioParameterFloat>("phaserBlend", "Wet/Dry", juce::NormalisableRange<float>(0.f, 1.f), 0.25f,"%"));
 
+        // Output Gain Params
+        layout.add(std::make_unique<juce::AudioParameterFloat>("outputGain", "Output", juce::NormalisableRange<float>(-20, 40, 0.1, 1), 0));
         return layout;
     }
     
@@ -110,7 +126,7 @@ public:
                 apTreeState.getRawParameterValue("LowCutSlope") -> load(),
                 apTreeState.getRawParameterValue("HighCutSlope") -> load());
 
-        (*dynamic_cast<CGainProcessor*>(gainNode -> getProcessor())).updateParams(apTreeState.getRawParameterValue("InputGain")-> load());
+        (*dynamic_cast<CGainProcessor*>(inputGainNode -> getProcessor())).updateParams(apTreeState.getRawParameterValue("inputGain")-> load());
         (*dynamic_cast<CReverbProcessor*>(reverbNode->getProcessor())).updateParams(
                 apTreeState.getRawParameterValue("rvbBlend")-> load(),
                 apTreeState.getRawParameterValue("rvbRoomSize")-> load(),
@@ -123,6 +139,21 @@ public:
                 apTreeState.getRawParameterValue("compRelease")->load(),
                 apTreeState.getRawParameterValue("compMakeupGain")->load()
                 );
+        (*dynamic_cast<CAmplifierIf*>(amplifierNode->getProcessor())).updateParams(
+                apTreeState.getRawParameterValue("ampDrive")->load(),
+                apTreeState.getRawParameterValue("ampBlend")->load(),
+                apTreeState.getRawParameterValue("ampType")->load(),
+                apTreeState.getRawParameterValue("ampPreLPF")->load(),
+                apTreeState.getRawParameterValue("ampPreHPF")->load(),
+                apTreeState.getRawParameterValue("ampEmphasis")->load(),
+                apTreeState.getRawParameterValue("ampEmphasisGain")->load(),
+                apTreeState.getRawParameterValue("ampPostLPF")->load(),
+                apTreeState.getRawParameterValue("ampPostHPF")->load()
+                );
+
+
+
+        (*dynamic_cast<CGainProcessor*>(outputGainNode -> getProcessor())).updateParams(apTreeState.getRawParameterValue("outputGain")-> load());
     }
 
     void prepareToPlay (double sampleRate, int samplesPerBlock) override
@@ -180,48 +211,27 @@ private:
         midiOutputNode  = mainProcessor->addNode (std::make_unique<AudioGraphIOProcessor> (AudioGraphIOProcessor::midiOutputNode));
         
         eqNode = mainProcessor->addNode (std::make_unique<EQ_v1AudioProcessor>());
-        gainNode = mainProcessor->addNode (std::make_unique<CGainProcessor>());
+        inputGainNode = mainProcessor->addNode (std::make_unique<CGainProcessor>());
         reverbNode = mainProcessor->addNode(std::make_unique<CReverbProcessor>());
         compressorNode = mainProcessor->addNode(std::make_unique<CCompressorProcessor>());
-
+        outputGainNode = mainProcessor->addNode(std::make_unique<CGainProcessor>());
+        amplifierNode = mainProcessor->addNode(std::make_unique<CAmplifierIf>());
         
         for (int channel = 0; channel < 2; ++channel)
         {
-            mainProcessor->addConnection ({ { audioInputNode->nodeID,         channel },
-                                            { eqNode->nodeID, channel } });
-            mainProcessor->addConnection ({ { eqNode->nodeID,  channel },
-                                            { audioOutputNode->nodeID,        channel } });
-            mainProcessor->addConnection ({ { midiInputNode->nodeID,         channel },
-                                            { eqNode->nodeID, channel } });
-            mainProcessor->addConnection ({ { eqNode->nodeID,  channel },
-                                            { midiOutputNode->nodeID,        channel } });
-            
-            mainProcessor->addConnection ({ { audioInputNode->nodeID,         channel },
-                                            { gainNode->nodeID, channel } });
-            mainProcessor->addConnection ({ { gainNode->nodeID,  channel },
-                                            { audioOutputNode->nodeID,        channel } });
-            mainProcessor->addConnection ({ { midiInputNode->nodeID,         channel },
-                                            { gainNode->nodeID, channel } });
-            mainProcessor->addConnection ({ { gainNode->nodeID,  channel },
-                                            { midiOutputNode->nodeID,        channel } });
-
-            mainProcessor->addConnection ({ { audioInputNode->nodeID,         channel },
-                                            { reverbNode->nodeID, channel } });
-            mainProcessor->addConnection ({ { reverbNode->nodeID,  channel },
-                                            { audioOutputNode->nodeID,        channel } });
-            mainProcessor->addConnection ({ { midiInputNode->nodeID,         channel },
-                                            { reverbNode->nodeID, channel } });
-            mainProcessor->addConnection ({ { reverbNode->nodeID,  channel },
-                                            { midiOutputNode->nodeID,        channel } });
-
-            mainProcessor->addConnection ({ { audioInputNode->nodeID,         channel },
-                                            { compressorNode->nodeID, channel } });
-            mainProcessor->addConnection ({ { compressorNode->nodeID,  channel },
-                                            { audioOutputNode->nodeID,        channel } });
-            mainProcessor->addConnection ({ { midiInputNode->nodeID,         channel },
-                                            { compressorNode->nodeID, channel } });
-            mainProcessor->addConnection ({ { compressorNode->nodeID,  channel },
-                                            { midiOutputNode->nodeID,        channel } });
+//            addConnectionToNode(eqNode->nodeID, inputGainNode->noteID, channel);
+//            addConnectionToNode(inputGainNode->nodeID, channel);
+//            addConnectionToNode(reverbNode->nodeID, channel);
+//            addConnectionToNode(compressorNode->nodeID, channel);
+//            addConnectionToNode(outputGainNode->nodeID, channel);
+            addConnectionToNode(audioInputNode->nodeID, inputGainNode->nodeID, channel);
+            addConnectionToNode(inputGainNode->nodeID, amplifierNode->nodeID, channel);
+            addConnectionToNode(amplifierNode->nodeID, eqNode->nodeID, channel);
+            addConnectionToNode(eqNode->nodeID, compressorNode->nodeID, channel);
+            addConnectionToNode(compressorNode->nodeID, reverbNode->nodeID, channel);
+            addConnectionToNode(reverbNode->nodeID, outputGainNode->nodeID, channel);
+            addConnectionToNode(outputGainNode->nodeID, audioOutputNode->nodeID, channel);
+//            addConnectionToNode(amplifierNode->nodeID, channel);
         }
         
         connectAudioNodes();
@@ -230,7 +240,15 @@ private:
         for (auto node : mainProcessor->getNodes())                 // [10]
             node->getProcessor()->enableAllBuses();
     }
-
+    void addConnectionToNode(juce::AudioProcessorGraph::NodeID currentNodeId, juce::AudioProcessorGraph::NodeID nextNodeId, int channel)
+    {
+        mainProcessor->addConnection ({{currentNodeId, channel},{nextNodeId, channel}});
+        mainProcessor->addConnection ({{currentNodeId, channel},{nextNodeId, channel}});
+//        mainProcessor->addConnection ({ { midiInputNode->nodeID,         channel },
+//                                        { nodeId, channel } });
+//        mainProcessor->addConnection ({ { nodeId,  channel },
+//                                        { midiOutputNode->nodeID,        channel } });
+    }
     void connectAudioNodes()
     {
         for (int channel = 0; channel < 2; ++channel)
@@ -254,9 +272,11 @@ private:
     Node::Ptr midiOutputNode;
     
     Node::Ptr eqNode;
-    Node::Ptr gainNode;
+    Node::Ptr inputGainNode;
     Node::Ptr reverbNode;
     Node::Ptr compressorNode;
+    Node::Ptr outputGainNode;
+    Node::Ptr amplifierNode;
 
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (GraphProcessor)
