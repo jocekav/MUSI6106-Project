ad95db808c0e92b9f9b1b6d3d6fbfbbba31829e1
jocekav <jkavanagh3@gatech.edu>
2022-05-01 22:08:41 -0400

    Prep for xml integration

---
+++ plugin_framework/ProcessorGraphTest/Source/PresetHandler.hpp
@@ -0,0 +1,13 @@
+//
+//  PresetHandler.hpp
+//  GUITARAMPPLUGIN
+//
+//  Created by Jocelyn Kavanagh on 5/1/22.
+//
+
+#ifndef PresetHandler_hpp
+#define PresetHandler_hpp
+
+#include <stdio.h>
+
+#endif /* PresetHandler_hpp */
